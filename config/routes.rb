c1cbd6c3e87ed78f53b911e95457d751c1f6ab68
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-23 16:41:06 -0700

    updated routes for chatrooms and messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get '/secret', to: 'home#secret', as: :secret
+  resources :chatrooms, only: [:show, :edit, :update]
+  resources :messages, only: [:create, :new, :show]
 end
