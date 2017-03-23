6372ddf608c4039de2294ac61e441e36e788f2f9
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-23 16:10:13 -0700

    added associations to chatroom model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,6 +2,8 @@ class Chatroom < ApplicationRecord
 	 
 	 default_scope -> {order(created_at: :desc)}
 
+	 has_many :messages, dependent: :destroy
+
 	 validates :title, presence: true, uniqueness: true, length: {in: 1..50}
 	 validates :description, length: {maximum: 100}
 end
