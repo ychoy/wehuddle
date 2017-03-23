88f619b16f549fb4ba35fa54703cb664ad0930f1
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-23 15:01:53 -0700

    added user info to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+users = [
+	{
+		first_name: "Will",
+		last_name: "Kaspar",
+		username: "catchat23" ,
+		email: "w@k.com",
+		password: "123456",
+		biography: "I'm a cat."
+	},
+	{
+		first_name: "Yan Yin",
+		last_name: "Choy",
+		username: "starcat",
+		email: "y@c.com",
+		password: "123456",
+		biography: "I'm a star-cat unicorn."
+	},
+	{
+		first_name: "Thelma",
+		last_name: "Boamah",
+		username: "thelma",
+		email: "t@b.com",
+		password: "123456",
+		biography: "I'm more of a goldfish."
+	},
+	{
+		first_name: "Kody",
+		last_name: "Lawton",
+		username: "kodycodes",
+		email: "k@l.com",
+		password: "123456",
+		biography: "I'm a kitten."
+	},
+	{
+		first_name: "Christen",
+		last_name: "Williams",
+		username: "chris10",
+		email: "c@w.com",
+		password: "123456",
+		biography: "I'm a puppy."
+	}
+]
+
+User.create(users)
