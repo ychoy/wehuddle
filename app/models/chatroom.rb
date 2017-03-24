e62698c09dee14df50eea286e4089bb24bb87d7a
Will Kaspar <Will@Aang.local>
2017-03-24 16:15:37 -0700

    added associations for memberships to chatroom and user models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,6 +3,7 @@ class Chatroom < ApplicationRecord
 	 default_scope -> {order(created_at: :desc)}
 
 	 has_many :messages, dependent: :destroy
+	 has_many :memberships, dependent: :destroy
 	 has_many :users, through: :memberships
 
 	 validates :title, presence: true, uniqueness: true, length: {in: 1..50}
