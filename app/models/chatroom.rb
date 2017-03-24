da8cd93efabf64eebbe25360cc5fe0f24788e436
Will Kaspar <Will@Aang.local>
2017-03-24 16:01:33 -0700

    adds membership model and proper associations on user/chatroom models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,9 +1,14 @@
 class Chatroom < ApplicationRecord
-	 
+
 	 default_scope -> {order(created_at: :desc)}
 
 	 has_many :messages, dependent: :destroy
+	 has_many :users, through: :memberships
 
 	 validates :title, presence: true, uniqueness: true, length: {in: 1..50}
 	 validates :description, length: {maximum: 100}
+
+	 def member?(user)
+		 user.chatrooms.include?(self)
+	 end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :messages
+  has_many :chatrooms, through: :memberships
 
 end
