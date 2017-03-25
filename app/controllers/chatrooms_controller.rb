43c78cd62b1afa9eb1a45e68af997ee5f1971fb7
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-24 17:02:10 -0700

    start editing controller for chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,10 @@
 class ChatroomsController < ApplicationController
+  before_action :set_chatroom, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @chatrooms = Chatroom.all
+  end
+
   def show
     #includes method for eager loading
     @chatroom = Chatroom.includes(:messages).find_by(id: params[:id])
@@ -7,11 +13,39 @@ class ChatroomsController < ApplicationController
     @message = Message.new
   end
 
+  def new
+    @chatroom = Chatroom.new
+  end
+
   def create
+    @chatroom = Chatroom.create(chatroom_params)
+    
     # TODO: Creates a new chatroom
     # make a chatroom
     # make a memmbership
     # set is_admin? to true
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def chatroom_params
+    params.require(:chatroom).permit(:title, :description)
   end
 
 end
