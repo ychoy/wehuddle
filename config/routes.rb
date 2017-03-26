6a3964e87fa56aef0bb1e491af77e3eeac8f2673
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-26 05:03:37 -0700

    update routes to make chatrooms index the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
-  get '/secret', to: 'home#secret', as: :secret
+  root to: 'chatrooms#index'
 
   mount ActionCable.server => '/cable'
 
-  resources :chatrooms, only: [:show, :edit, :update]
+  resources :chatrooms
   resources :messages, only: [:create, :new, :show]
 end
