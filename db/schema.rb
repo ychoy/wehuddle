6eeaaad426a3ccf226275b64ba6ca770cc16e3f8
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 10:47:00 -0700

    updated schema with DM to chatrooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324234858) do
+ActiveRecord::Schema.define(version: 20170327153239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "add_direct_messageto_chatrooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "chatrooms", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "admin"
+    t.boolean  "direct_message", default: false
   end
 
   create_table "memberships", force: :cascade do |t|
