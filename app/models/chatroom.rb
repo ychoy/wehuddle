b1a1a6258a9bc856a549c528fc649cff95e943ab
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 10:46:39 -0700

    add public channels and dm scope

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -6,10 +6,15 @@ class Chatroom < ApplicationRecord
 	 has_many :memberships, dependent: :destroy
 	 has_many :users, through: :memberships
 
+	 scope :public_channels, ->{ where(direct_message: false) }
+	 scope :direct_messages, ->{ where(direct_message: true) }
+
 	 validates :title, presence: true, uniqueness: true, length: {in: 1..50}
 	 validates :description, length: {maximum: 100}
+
 
 	 def member?(user)
 		 user.chatrooms.include?(self)
 	 end
+
 end
