cc5be1b8ca325ac235d291d42b89b4ecf0f3e620
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-26 20:44:24 -0700

    updated index and create methods

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -5,9 +5,14 @@ class MembershipsController < ApplicationController
     @chatrooms = Chatroom.find_by_id(@user)
   end
 
-  def join
-    @chatroom = Chatroom.find(params[:id])
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    #membership = Membership.new(user_id: current_user.id, chatroom_id: @chatroom.id)
+    #if membership.save
+    #  @#chatroom.users.push(current_user)
     @chatroom.users.push(current_user) unless @chatroom.member?(current_user)
+    redirect_to chatroom_path(@chatroom)
+
   end
 
 end
