9485b9282f2eab073b6de72ef5e0a5bd5d6785c0
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-26 22:28:59 -0700

    added tests for chatrooms controller

--- spec/controllers/chatrooms_controller_spec.rb
+++ spec/controllers/chatrooms_controller_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe ChatroomsController, type: :controller do
+RSpec.describe ChatroomsController, :type => :controller do
+
+  describe "GET #new" do
+      it "renders the new chatrooms page" do
+        # this line simulates a "GET" request to the chatrooms controller
+        # that hits the #new method, passing in `{chatroom: {}}` as params.
+        get :new, chatroom: {}
+        # check to make sure that the response renders back the new template
+        expect(response).to render_template("new")
+        expect(response).to have_http_status(200)
+      end
+    end
+
+
+  describe "POST #create" do
+
+    context "with invalid params" do
+      it "validates the presence of title and body" do
+        post :create, chatroom: {title: "this is an invalid chatroom"}
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects to the chatroom show page" do
+        post :create, chatroom: {title: "teehee", description: "cats!"}
+        expect(response).to redirect_to(chatroom_path(chatroom.last))
+      end
+    end
+  end
 
 end
