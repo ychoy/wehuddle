64eedb5ea06c448a044a1d5dd7436a05487a2aa4
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 15:07:52 -0700

    render new message form

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -5,6 +5,7 @@ class DirectMessagesController < ApplicationController
     users = [current_user, User.find(params[:id])]
     @chatroom = Chatroom.direct_message_for_users(users)
     @chatrooms = Chatroom.all
+    @message = Message.new 
     @messages = @chatroom.messages.order(created_at: :desc).limit(100).reverse
     @chatroom_user = current_user.memberships.find_by(chatroom_id: @chatroom.id)
     render "chatrooms/show"
