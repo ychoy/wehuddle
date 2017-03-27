5427c7b843bb4a1d275e97bd9ee3ddc4858207cb
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 13:24:01 -0700

    defined show method for DM controller

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -4,8 +4,9 @@ class DirectMessagesController < ApplicationController
   def show
     users = [current_user, User.find(params[:id])]
     @chatroom = Chatroom.direct_message_for_users(users)
+    @chatrooms = Chatroom.all
     @messages = @chatroom.messages.order(created_at: :desc).limit(100).reverse
-    @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
+    @chatroom_user = current_user.memberships.find_by(chatroom_id: @chatroom.id)
     render "chatrooms/show"
     end
 end
