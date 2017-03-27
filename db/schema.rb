f0f4b71b415dde10045df139f70023abbb7d1e61
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 12:12:48 -0700

    added dm to chatrooms

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20170327153239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "add_direct_messageto_chatrooms", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "chatrooms", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
