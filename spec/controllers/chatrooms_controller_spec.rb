0e3bf27174d4646f6089816ea88a243767a93082
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-27 10:46:49 -0700

    update spec

--- spec/controllers/chatrooms_controller_spec.rb
+++ spec/controllers/chatrooms_controller_spec.rb
@@ -18,7 +18,10 @@ RSpec.describe ChatroomsController, :type => :controller do
 
     context "with invalid params" do
       it "validates the presence of title and body" do
-        post :create, chatroom: {title: "this is an invalid chatroom"}
+        post :create, chatroom: {title: "this is an invalid chatroom", description: "cats!"}
+        #BUG - doesn't recognize chatroom id
+        post :create, membership: {user_id => current_user.id, chatroom_id => @chatroom_id}
+
         expect(response).to render_template("new")
         expect(flash[:errors]).to be_present
       end
@@ -27,6 +30,9 @@ RSpec.describe ChatroomsController, :type => :controller do
     context "with valid params" do
       it "redirects to the chatroom show page" do
         post :create, chatroom: {title: "teehee", description: "cats!"}
+        #BUG - doesn't recognize chatroom id
+        post :create, membership: {user_id => current_user.id, chatroom_id => @chatroom_id}
+
         expect(response).to redirect_to(chatroom_path(chatroom.last))
       end
     end
