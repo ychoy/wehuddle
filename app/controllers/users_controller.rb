9db93144148cee837aa11d755847d5a4a269eb0a
Will Kaspar <Will@Aang.local>
2017-03-28 14:27:24 -0700

    side nav bar now persists through navigation away from chatrooms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,13 @@ class UsersController < ApplicationController
   def show
   	@user = User.find_by_id(params[:id])
     @memberships = @user.memberships
+
+    # allows sidebar menu to populate
+    @chatrooms = Chatroom.all
+    #includes method for eager loading
+    @chatroom = Chatroom.includes(:messages).find_by(id: params[:id])
+
+    #allows new messages to be created
+    @message = Message.new
   end
 end
