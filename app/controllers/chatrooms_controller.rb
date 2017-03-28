960a5d987d857467117b0f929d71126692ddfc51
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-28 15:22:54 -0700

    removed old comment

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -21,8 +21,6 @@ class ChatroomsController < ApplicationController
 
   def create
     @chatroom = Chatroom.create(chatroom_params)
-    #Sets admin of chat to user creating it
-    #@#chatroom.admin = current_user #BUG, NOT WORKING
 
     if @chatroom.save
       #Creates a new membership between admin and chatroom
