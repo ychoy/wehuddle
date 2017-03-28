3c3597f56f591741be2d2d811999b49be4b88c13
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-28 07:32:45 -0700

    updated configured user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,4 +1,13 @@
 FactoryGirl.define do
+  sequence :first_name do |n|
+    "person#{n}"
+  end
+  sequence :last_name do |n|
+    "person#{n}"
+  end
+  sequence :username do |n|
+    "person#{n}"
+  end
   sequence :email do |n|
     "person#{n}@example.com"
   end
@@ -6,6 +15,9 @@ end
 
 FactoryGirl.define do
   factory :user, :class => 'User' do
+    first_name
+    last_name
+    username
     email
     password '12345678'
     password_confirmation '12345678'
